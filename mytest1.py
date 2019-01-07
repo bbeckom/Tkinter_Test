7ec3964e99eafb53b209e13bbf9720d3800e4b96
Ben Beckom <benbeckom@gmail.com>
2019-01-06 20:16:18 -0700

    added functionality to mytest1.py

--- mytest1.py
+++ mytest1.py
@@ -1,7 +1,9 @@
 import tkinter
 
+def SimplePrint():
+    print("nothing")
 
-class SampleApp(tkinter.Tk):
+class MyApp(tkinter.Tk):
     def __init__(self):
 
         tkinter.Tk.__init__(self)
@@ -9,10 +11,22 @@ class SampleApp(tkinter.Tk):
         self.button = tkinter.Button(self, text="Get Input", command=self.on_button)
         self.button.pack()
         self.entry.pack()
+        self.menucreate()
 
     def on_button(self):
         print(self.entry.get())
 
+    def menucreate(self):
+        self.mymenu = tkinter.Menu(self)
+        self.config(menu=self.mymenu)
 
-app = SampleApp()
+        self.subMenu = tkinter.Menu(self.mymenu, tearoff=0)
+        self.mymenu.add_cascade(label="File", menu=self.subMenu)
+
+        self.subMenu.add_command(label="Print text", command=SimplePrint)
+        self.subMenu.add_separator()
+        self.subMenu.add_command(label="Exit", command=self.quit)
+
+
+app = MyApp()
 app.mainloop()
