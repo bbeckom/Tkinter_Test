993cdf2ff00f3e378476dd7fa38f0f4ba2886a8c
Ben Beckom <benbeckom@gmail.com>
2019-01-06 22:51:51 -0700

    1/6

--- mytest1.py
+++ mytest1.py
@@ -4,20 +4,25 @@ import tkinter
 
 
 # simple function to test commands for menu selections quickly
-def SimplePrint():
-    # call the app variable that is created upon execution and change statusbar text to static string
+def SimplePrint(val=''):
+    # call the app variable that is created upon execution and change statusbar and canvas text to static string
     app.statusbar.config(text="Text printed")
+    app.logoCanvas.itemconfigure(app.logoText, text="Text printed")
     print("Text printed")
 
 
 class MyApp(tkinter.Tk):
     def __init__(self):
         tkinter.Tk.__init__(self)
-        # create frame sections
+        # create frame sections for pack layout
         self.topFrame = tkinter.Frame(self)
         self.topFrame.pack(fill=tkinter.X)
         self.bottomFrame = tkinter.Frame(self)
         self.bottomFrame.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+        # put in logo
+        self.myLogo = self.myCanvas()
+        self.logoCanvas = self.myLogo[0]
+        self.logoText = self.myLogo[1]
         # entry field
         self.entry = tkinter.Entry(self.topFrame)
         # button for entry field
@@ -26,15 +31,23 @@ class MyApp(tkinter.Tk):
         self.getButton.pack(side=tkinter.RIGHT)
         self.entry.pack(side=tkinter.LEFT, fill=tkinter.X, expand=True)
         # create menu
-        self.menucreate()
+        self.mainMenu = self.menucreate()
         # create status bar
         self.statusbar = self.statuscreate()
+        # bind enter key to push getButton
+        self.bind('<Return>', self.on_button)
+        # print init text to console
         print("init text")
 
-    def on_button(self):
+    def on_button(self, event=''):
         print(self.entry.get())
         # reconfigure statusbar to display entry text
         self.statusbar.config(text=self.entry.get())
+        # reconfigure logo text object, I returned the canvas and text objects created in myCanvas
+        logoCanvas = self.logoCanvas
+        logoText = self.logoText
+        logoCanvas.itemconfigure(logoText, text=self.entry.get())
+
 
     def menucreate(self):
         # create menu and set to app with self.config
@@ -53,6 +66,21 @@ class MyApp(tkinter.Tk):
         self.status.pack(fill=tkinter.X)
         # return so we can update this object later
         return self.status
+
+    def myCanvas(self):
+        # create logo space
+        self.canvas = tkinter.Canvas(self.topFrame, width=600, height=480)
+        self.canvas.pack()
+        # just a bunch of random objects drawn
+        self.canvas.create_rectangle(0, 0, 595, 475, fill="red")
+        self.canvas.create_rectangle(5, 5, 600, 480, fill="blue")
+        self.canvas.create_line(0, 0, 600, 480, width=20)
+        self.canvas.create_line(600, 0, 0, 480, width=20)
+        self.canvas.create_line(600, 240, 0, 240, width=20)
+        self.canvas.create_line(300, 0, 300, 480, width=20)
+        # create initial text
+        self.text_1 = self.canvas.create_text(200, 200, text="init text", font=("Purisa",22), fill="white")
+        return self.canvas, self.text_1
 
 
 # start app
--- test_files/13.py
+++ test_files/13.py
@@ -11,6 +11,6 @@ redLine = canvas.create_line(200, 0, 0, 100, fill="red")
 greenBox = canvas.create_rectangle(75, 25, 125, 50, fill="blue")
 
 canvas.delete(redLine)
-# to delete all... canvas.delete(ALL)
+# to delete all... canvas.delete(tkinter.ALL)
 
 root.mainloop()
