72818506511ac6a2b57d5669a945a972797b7271
Ben Beckom <benbeckom@gmail.com>
2019-01-06 21:03:04 -0700

    added comments to mytest1.py

--- mytest1.py
+++ mytest1.py
@@ -9,19 +9,26 @@ def SimplePrint():
 class MyApp(tkinter.Tk):
     def __init__(self):
         tkinter.Tk.__init__(self)
+        # create frame sections
         self.topFrame = tkinter.Frame(self)
         self.topFrame.pack(fill=tkinter.X)
         self.bottomFrame = tkinter.Frame(self)
         self.bottomFrame.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+        # entry field
         self.entry = tkinter.Entry(self.topFrame)
+        # button for entry field
         self.getButton = tkinter.Button(self.topFrame, text="Get Input", command=self.on_button)
+        # pack button and entry field
         self.getButton.pack(side=tkinter.RIGHT)
         self.entry.pack(side=tkinter.LEFT, fill=tkinter.X, expand=True)
+        # create menu
         self.menucreate()
+        # create status bar
         self.statusbar = self.statuscreate()
 
     def on_button(self):
         print(self.entry.get())
+        # reconfigure statusbar to display entry text
         self.statusbar.config(text=self.entry.get())
 
     def menucreate(self):
@@ -39,8 +46,10 @@ class MyApp(tkinter.Tk):
     def statuscreate(self):
         self.status = tkinter.Label(self.bottomFrame, text='init', bd=1, relief=tkinter.SUNKEN, anchor=tkinter.W)
         self.status.pack(fill=tkinter.X)
+        # return so we can update this object later
         return self.status
 
 
+# start app
 app = MyApp()
 app.mainloop()
