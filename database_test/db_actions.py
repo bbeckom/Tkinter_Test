457b3dda779f3da2703413c963f40459c4779ba6
Ben Beckom <benbeckom@gmail.com>
2019-01-14 22:00:06 -0700

    minor formatting changes

--- database_test/db_actions.py
+++ database_test/db_actions.py
@@ -7,8 +7,8 @@ cursor = db.cursor()
 
 def add_name(name, val=''):
     name = str(name)
-    add = cursor.execute("INSERT INTO NAME_table (NAME)"
-                   "VALUES ('%s')" % name)
+    cursor.execute("INSERT INTO NAME_table (NAME)"
+                         "VALUES ('%s')" % name)
     db.commit()
     added_name = cursor.execute("SELECT * FROM NAME_table WHERE NAME='%s' ORDER BY ID DESC LIMIT 1" % name)
     return added_name
@@ -24,17 +24,17 @@ def list_individual(name, val=''):
     names = cursor.execute("SELECT * FROM NAME_table WHERE NAME='%s'" % name)
     return names
 
+
 def remove_name(name, val=''):
     name = str(name)
-    remove = cursor.execute("DELETE FROM NAME_table WHERE NAME='%s'" % name)
+    cursor.execute("DELETE FROM NAME_table WHERE NAME='%s'" % name)
     db.commit()
     names = cursor.execute("SELECT * FROM NAME_table")
     return names
 
 
-
-#remove_name("test")
-#add_name("test")
+# remove_name("test")
+# add_name("test")
 
 names = list_names()
 for na in names:
